5d8527fd8117122212347f6c31a7b5a483c9e0df
ohletamyleta <ohletamyleta@gmail.com>
2021-05-19 14:14:08 -0400

    building form

--- src/containers/spellForm.js
+++ src/containers/spellForm.js
@@ -1,2 +1,52 @@
 //I will be the create/edit a spell form
 //MAKE SURE TO ADD A NOTE ABOUT SPECIFYING HOMEBREW IN THE SPELL DESCRIPTION!!!
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
+
+import { updateSpell, createSpell } from '../actions/spellActions';
+
+class spellForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      desc: '',
+      higher_level: '',
+      range: '',
+      components: '',
+      material: '',
+      ritual: '',
+      duration: '',
+      concentration: '',
+      casting_time: '',
+      level: '',
+      level_int: '',
+      school: '',
+      dnd_class: '',
+      archetype: '',
+      circles: '',
+    };
+
+    this.baseState = this.state;
+  }
+
+  componentDidMount = () => {
+    const { name } = this.state;
+    const { spell } = this.props;
+
+    if (!name) {
+      this.setState(prevState => ({ ...prevState, ...spell}));
+    }
+  };
+
+
+    render() {
+      return(
+        <form>
+          
+        </form>
+      )
+    }
+}
