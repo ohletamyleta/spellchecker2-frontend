8ae589965bcc918dd9b348f77bad3e8569318a78
ohletamyleta <ohletamyleta@gmail.com>
2021-05-19 09:51:14 -0400

    install redux and material-ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+
 import { Provider } from 'react-redux';
+import store from './Store';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
