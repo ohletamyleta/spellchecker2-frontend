a7979c11b44497a40cfc6146602ebf24e9cc6448
ohletamyleta <ohletamyleta@gmail.com>
2021-05-26 18:01:11 -0400

    build spell detail page

--- src/components/SpellDetail.js
+++ src/components/SpellDetail.js
@@ -1,36 +1,45 @@
 import React, { useState, useEffect } from 'react';
-
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getSpells } from '../components/services';
 
 const SpellDetail = () => {
 
   const [spells, setSpells] = useState([]);
-  const params = useParams();
-  
+  // const params = useParams();
+  const useParamsInt = (key = 'id') => {
+    const params = useParams();
+    return params[key] ? parseInt(params[key]) : null;
+  };
+
+  const spellId = useParamsInt();
+
 
   useEffect(() => {
     let mounted = true;
     getSpells()
       .then(spells => {
         if(mounted) {
-          setSpells(spells)
+          setSpells([spells.spells])
         }
       })
       return () => mounted = false;
   }, [])
 
-  console.log(spells);
+  
 
+  console.log(spellId);
 
-
-
+  function findSpell(spellId) {
+    return spells.data.filter(spell => {
+      return spell.id === spellId
+    })
+  };
 
   return (
     <div className="detail-card">
-    {spells.data.filter(spell => spell.data.id === params).map(filteredSpell =>
-    <li>{filteredSpell.name}</li>)}
-      <h2> THERE!</h2>
+    
+      <h2>OY!{findSpell()}</h2>
       
     </div>
 
